926bc7cd94e6ba884e83f2c22698f9d85e6eb24b
Lisa Fung <lisafung710@gmail.com>
2024-12-03 23:22:29 -0800

    Finish initial MDP and MCTS implementation and run MCTS in online planning

--- mdp_energy_grid.jl
+++ mdp_energy_grid.jl
@@ -25,7 +25,6 @@ struct EnergyGridAction
     edge::Union{Tuple{Int, Int}, Nothing}   # Specify 2 vertices for :addEdge action
 end
 
-
 # EnergyGridMDP type
 struct EnergyGridMDP <: MDP{EnergyGridState, EnergyGridAction}
     g_init::Graph   # Intial grid configuration
@@ -38,6 +37,7 @@ end
 function rewards_default(s::EnergyGridState, a::EnergyGridAction)
     # State
     r_s = nv(s.g)   # More supply/storage vertices, the better
+    r_a = 0
 
     # Action
     if a.action == :addSupply
@@ -51,8 +51,9 @@ function rewards_default(s::EnergyGridState, a::EnergyGridAction)
     return r_s + r_a
 end
 
-# Default Transition function T(s' | s, a)
-function transitions_default(s::EnergyGridState, a::EnergyGridAction)
+# v1 Default Transition function T(s' | s, a)
+# Directly updates state s to s', and returns s
+function transition_state(s::EnergyGridState, a::EnergyGridAction)
     likelihood = rand()
     if a.action == :addSupply && likelihood < 0.8
         add_vertex!(s.g)
@@ -61,6 +62,24 @@ function transitions_default(s::EnergyGridState, a::EnergyGridAction)
     elseif a.action == :addEdge && likelihood < 0.6
         add_edge!(s.g, a.edge[1], a.edge[2])
     end
+    return s
+end
+
+# Default Transition function T(s' | s, a)
+# Returns a SparseCat (categorical distribution) instead of s'
+
+function transitions_default(s::EnergyGridState, a::EnergyGridAction)
+    g_copy = copy(s.g)
+    action_probs = Dict(:addSupply => 0.8, :addStorage => 0.7, :addEdge => 0.6, :noAction => 1.0)
+    if a.action in [:addSupply, :addStorage]
+        add_vertex!(g_copy)
+    elseif a.action == :addEdge
+        add_edge!(g_copy, a.edge[1], a.edge[2])
+    elseif a.action == :noAction
+    else
+        error("Invalid action")
+    end
+    return SparseCat([EnergyGridState(g_copy), EnergyGridState(copy(s.g))], [action_probs[a.action], 1 - action_probs[a.action]])
 end
 
 # EnergyGridMDP constructor
@@ -72,6 +91,58 @@ function EnergyGridMDP(;
     return EnergyGridMDP(g_init, rewards, transitions, discount_factor)
 end
 
+# Skip state space (too large, using online planning techniques, e.g. MCTS)
+
+# Actions from specific state s
+POMDPs.actions(mdp::EnergyGridMDP, s::EnergyGridState) = 
+    vcat([EnergyGridAction(a, nothing) for a in [:addSupply, :addStorage, :noAction]], 
+         [EnergyGridAction(:addEdge, (i, j)) for i in 1:nv(s.g) for j in i+1:nv(s.g)]...)
+# Note: allow adding edges that already exist in s.g::Graph for now, will not change graph
+
+# Return action index
+function POMDPs.actionindex(mdp::EnergyGridMDP, a::EnergyGridAction)
+    @assert in(a, actions(mdp)) "Invalid action"
+    return findfirst(x -> x == a, actions(mdp))
+end
+
+# Transition function
+# Returns the transition distribution from the current state-action pair.
+POMDPs.transition(mdp::EnergyGridMDP, s::EnergyGridState, a::EnergyGridAction) = mdp.transitions(s, a)
+
+# Reward function (simple)
+POMDPs.reward(mdp::EnergyGridMDP, s::EnergyGridState, a::EnergyGridAction) = mdp.rewards(s, a)
+
+# Discount
+function POMDPs.discount(mdp::EnergyGridMDP)
+    return mdp.discount_factor
+end
 
 
+"""
+Test POMDP code
+"""
+mdp = EnergyGridMDP()
+# print(actions(mdp, EnergyGridState(SimpleGraph(5))))
 
+solver = MCTSSolver(n_iterations=100, depth=10, exploration_constant=10.0)
+mcts_planner = POMDPs.solve(solver, mdp)
+
+s0 = EnergyGridState(SimpleGraph(5))
+s = s0
+num_steps = 20
+action_order = Vector{EnergyGridAction}()
+for i in 1:num_steps
+    println("Step ", i)
+    a = action(mcts_planner, s)
+    println("Action a: ", a)
+    push!(action_order, a)
+    global s = transition_state(s, a)
+    println("New state sp: ", s)
+    println()
+end
+
+println("List of actions: ", [(i, a.action) for (i, a) in enumerate(action_order)])
+
+# Debugging outputs:
+# print(mcts_planner.rng)
+# print(@gen(:sp, :r)(mdp, s0, EnergyGridAction(:addSupply, nothing), mcts_planner.rng))
