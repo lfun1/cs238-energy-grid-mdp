7b327494c8b8b9037133d365e7664f74d603c3aa
Lisa Fung <lisafung710@gmail.com>
2024-12-05 21:49:36 -0800

    Refactor code, reformulate reward with reliability and num vertices, adjust state and transitions with budget

--- mdp_energy_grid.jl
+++ mdp_energy_grid.jl
@@ -17,6 +17,7 @@ using POMDPs, POMDPTools, MCTS
 # EnergyGrid State
 struct EnergyGridState
     g::Graph    # graph of energy grid configuration
+    budget::Int64   # Budget remaining to add vertices or edges
 end
 
 # EnergyGrid Actions
@@ -27,13 +28,16 @@ end
 
 # EnergyGridMDP type
 struct EnergyGridMDP <: MDP{EnergyGridState, EnergyGridAction}
-    g_init::Graph   # Intial grid configuration
-    rewards::Function       # Reward function mapping states and actions to reward (params: number of storage/supply vertices)
-    transitions::Function   # Transition function that modifies state based on action with certain probabilities
-    discount_factor::Float64 # disocunt factor
+    g_init::Graph   # Intial grid configuration, includes initial number of vertices
+    action_cost::Dict{Symbol, Int64}   # Dictionary mapping action symbol to cost
+    reward_reliability::Function    # Function to evaluate reliability of state's graph (Karger's Algorithm)
+    reward_alpha::Float64   # Relative weighting of reliability vs number of vertices in reward
+    discount_factor::Float64 # discount factor
+    # rewards::Function       # Reward function mapping states and actions to reward (params: number of storage/supply vertices)
+    # transitions::Function   # Transition function that modifies state based on action with certain probabilities
 end
 
-# Default Reward function R(s, a)
+# v1 Default Reward function R(s, a)
 function rewards_default(s::EnergyGridState, a::EnergyGridAction)
     # State
     r_s = nv(s.g)   # More supply/storage vertices, the better
@@ -54,6 +58,7 @@ end
 # v1 Default Transition function T(s' | s, a)
 # Directly updates state s to s', and returns s
 function transition_state(s::EnergyGridState, a::EnergyGridAction)
+    next_state_prob = POMDPs.transition(mdp, s::EnergyGridState, a::EnergyGridAction)
     likelihood = rand()
     if a.action == :addSupply && likelihood < 0.8
         add_vertex!(s.g)
@@ -65,9 +70,8 @@ function transition_state(s::EnergyGridState, a::EnergyGridAction)
     return s
 end
 
-# Default Transition function T(s' | s, a)
+# v2 Default Transition function T(s' | s, a)
 # Returns a SparseCat (categorical distribution) instead of s'
-
 function transitions_default(s::EnergyGridState, a::EnergyGridAction)
     g_copy = copy(s.g)
     action_probs = Dict(:addSupply => 0.8, :addStorage => 0.7, :addEdge => 0.6, :noAction => 1.0)
@@ -82,13 +86,37 @@ function transitions_default(s::EnergyGridState, a::EnergyGridAction)
     return SparseCat([EnergyGridState(g_copy), EnergyGridState(copy(s.g))], [action_probs[a.action], 1 - action_probs[a.action]])
 end
 
+"""
+Function to run Karger's Algorithm for many trials to achieve higher probability of success.
+    Only applies to unweighted undirected graphs with at most 1 edge between vertices.
+    Uses Graphs.karger_min_cut(g) to for each run of Karger's Algorithm.
+"""
+function karger_multiple_min_cut(g::Graph; prob_fail::Float64=0.01)
+    # Given delta = probability of failure
+    # Number of trials T >= n^2 * ln(1/delta) >= (n choose 2) * ln(1/deta)
+    num_trials = trunc(Int, nv(g) * nv(g) * log(1/prob_fail)) + 1
+    min_cut_cost = nv(g) * (nv(g) - 1)   # Maximum cut value = maximum number of edges
+
+    # Repeat Karger's algorithm num_trials times
+    for i in 1:num_trials
+        cut = karger_min_cut(g)
+        cut_cost = karger_cut_cost(g, cut)
+        min_cut_cost = min(min_cut_cost, cut_cost)
+    end
+
+    return min_cut_cost
+end
+
 # EnergyGridMDP constructor
 function EnergyGridMDP(;
-    g_init::Graph = Graph(),
-    rewards::Function = rewards_default,
-    transitions::Function = transitions_default,
+    g_init::Graph = SimpleGraph(),
+    action_cost::Dict{Symbol, Int64} = Dict(
+        :addSupply => 1, :addStorage => 3, :addEdge => 1, :noAction => 0
+    ),
+    reward_reliability::Function = karger_multiple_min_cut,
+    reward_alpha::Float64 = 0.7,
     discount_factor::Float64=0.9)
-    return EnergyGridMDP(g_init, rewards, transitions, discount_factor)
+    return EnergyGridMDP(g_init, action_cost, reward_reliability, reward_alpha, discount_factor)
 end
 
 # Skip state space (too large, using online planning techniques, e.g. MCTS)
@@ -107,10 +135,28 @@ end
 
 # Transition function
 # Returns the transition distribution from the current state-action pair.
-POMDPs.transition(mdp::EnergyGridMDP, s::EnergyGridState, a::EnergyGridAction) = mdp.transitions(s, a)
+function POMDPs.transition(mdp::EnergyGridMDP, s::EnergyGridState, a::EnergyGridAction) 
+    g_copy = copy(s.g)
+    budget_remain = s.budget - mdp.action_cost[a.action]
+    action_probs = Dict(:addSupply => 0.8, :addStorage => 0.7, :addEdge => 0.6, :noAction => 1.0)
+    if a.action == :noAction || budget_remain < 0
+        # State remains same, no action taken
+        return SparseCat([EnergyGridState(g_copy, s.budget)], [1.0])
+    elseif a.action in [:addSupply, :addStorage]
+        add_vertex!(g_copy)
+    elseif a.action == :addEdge
+        add_edge!(g_copy, a.edge[1], a.edge[2])
+    else
+        error("Invalid action")
+    end
+    return SparseCat([EnergyGridState(g_copy, budget_remain), EnergyGridState(copy(s.g), budget_remain)], 
+                    [action_probs[a.action], 1 - action_probs[a.action]])
+end
 
-# Reward function (simple)
-POMDPs.reward(mdp::EnergyGridMDP, s::EnergyGridState, a::EnergyGridAction) = mdp.rewards(s, a)
+# Reward function: based on reliability, number of vertices, action
+function POMDPs.reward(mdp::EnergyGridMDP, s::EnergyGridState, a::EnergyGridAction)
+    return mdp.reward_alpha * mdp.reward_reliability(s.g) + (1-mdp.reward_alpha) * nv(s.g)
+end
 
 # Discount
 function POMDPs.discount(mdp::EnergyGridMDP)
@@ -127,21 +173,24 @@ mdp = EnergyGridMDP()
 solver = MCTSSolver(n_iterations=100, depth=10, exploration_constant=10.0)
 mcts_planner = POMDPs.solve(solver, mdp)
 
-s0 = EnergyGridState(SimpleGraph(5))
+s0 = EnergyGridState(SimpleGraph(5), 12)
 s = s0
-num_steps = 20
+total_reward = 0
+num_steps = 10
 action_order = Vector{EnergyGridAction}()
 for i in 1:num_steps
     println("Step ", i)
     a = action(mcts_planner, s)
     println("Action a: ", a)
     push!(action_order, a)
-    global s = transition_state(s, a)
+    global s, r = @gen(:sp, :r)(mdp, s, a, mcts_planner.rng)
+    global total_reward += r
     println("New state sp: ", s)
     println()
 end
 
 println("List of actions: ", [(i, a.action) for (i, a) in enumerate(action_order)])
+println("Total reward: ", total_reward)
 
 # Debugging outputs:
 # print(mcts_planner.rng)
