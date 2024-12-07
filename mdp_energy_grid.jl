c3697df3881a8d48ad67b405c35101dd9fa23225
Lisa Fung <lisafung710@gmail.com>
2024-12-07 12:17:07 -0800

    Add initial experiment evaluation and plotting

--- mdp_energy_grid.jl
+++ mdp_energy_grid.jl
@@ -31,7 +31,8 @@ struct EnergyGridMDP <: MDP{EnergyGridState, EnergyGridAction}
     g_init::Graph   # Intial grid configuration, includes initial number of vertices
     action_cost::Dict{Symbol, Int64}   # Dictionary mapping action symbol to cost
     reward_reliability::Function    # Function to evaluate reliability of state's graph (Karger's Algorithm)
-    reward_alpha::Float64   # Relative weighting of reliability vs number of vertices in reward
+    reward_alpha::Float64   # Relative weighting of reliability vs budget in reward
+    reward_beta::Float64   # Relative number of vertices vs budget in reward
     discount_factor::Float64 # discount factor
     # rewards::Function       # Reward function mapping states and actions to reward (params: number of storage/supply vertices)
     # transitions::Function   # Transition function that modifies state based on action with certain probabilities
@@ -114,9 +115,10 @@ function EnergyGridMDP(;
         :addSupply => 1, :addStorage => 1, :addEdge => 3, :noAction => 0
     ),
     reward_reliability::Function = karger_multiple_min_cut,
-    reward_alpha::Float64 = 10.0,
+    reward_alpha::Float64 = 30.0,
+    reward_beta::Float64 = 4.0,
     discount_factor::Float64=0.9)
-    return EnergyGridMDP(g_init, action_cost, reward_reliability, reward_alpha, discount_factor)
+    return EnergyGridMDP(g_init, action_cost, reward_reliability, reward_alpha, reward_beta, discount_factor)
 end
 
 # Skip state space (too large, using online planning techniques, e.g. MCTS)
@@ -141,7 +143,7 @@ function POMDPs.transition(mdp::EnergyGridMDP, s::EnergyGridState, a::EnergyGrid
     action_probs = Dict(:addSupply => 0.8, :addStorage => 0.7, :addEdge => 0.6, :noAction => 1.0)
     if a.action == :noAction || budget_remain < 0
         # State remains same, no action taken
-        return SparseCat([EnergyGridState(g_copy, s.budget)], [1.0])
+        return SparseCat([EnergyGridState(g_copy, budget_remain)], [1.0])
     elseif a.action in [:addSupply, :addStorage]
         add_vertex!(g_copy)
     elseif a.action == :addEdge
@@ -154,11 +156,18 @@ function POMDPs.transition(mdp::EnergyGridMDP, s::EnergyGridState, a::EnergyGrid
 end
 
 # Reward function: based on reliability, number of vertices, action
+# Added budget to for proper spending behavior
 function POMDPs.reward(mdp::EnergyGridMDP, s::EnergyGridState, a::EnergyGridAction)
     r_reliability = mdp.reward_reliability(s.g)
     r_vertices = nv(s.g)
-    # println("Reward components: min cut = ", r_reliability, ", vertices = ", r_vertices)
+
+    # Initial reward function:
     return mdp.reward_alpha * r_reliability + r_vertices
+
+    # Budget reward function:
+    
+    # println("Reward components: min cut = ", r_reliability, ", vertices = ", r_vertices)
+    return mdp.reward_alpha * r_reliability + mdp.reward_beta * r_vertices + s.budget
 end
 
 # Discount
@@ -170,88 +179,170 @@ end
 """
 Evaluate MDP with MCTS
 """
-mdp = EnergyGridMDP(reward_alpha=5.0)    # Weight min_cut 10x as much as num vertices
-solver = MCTSSolver(n_iterations=100, depth=10, exploration_constant=10.0)
-mcts_planner = POMDPs.solve(solver, mdp)
 
-s0 = EnergyGridState(SimpleGraph(), 50)     # Budget
-num_steps = 20
-s = s0
+# Single full run of MCTS online planning
+function evaluate_mdp_2a(mdp::EnergyGridMDP, s0::EnergyGridState, num_steps::Int64, )
+    solver = MCTSSolver(n_iterations=100, depth=10, exploration_constant=10.0)
+    mcts_planner = POMDPs.solve(solver, mdp)
 
-# Metrics to track
-metrics::Dict{Symbol, Any} = Dict(
-    :reliability => 0,
-    :total_cost => 0,
-    :total_steps => 0,
-    :max_degree => 0,
-    :total_reward => 0,
-    :discounted_reward => 0    
-)
+    # Metrics to track
+    metrics::Dict{Symbol, Any} = Dict(
+        :reliability => 0,
+        :total_cost => 0,
+        :total_steps => 0,
+        :max_degree => 0,
+        :total_reward => 0,
+        :discounted_reward => 0,
+        :final_graph => SimpleGraph()
+    )
 
-action_order = Vector{EnergyGridAction}()
+    s = s0
 
-for i in 1:num_steps
-    println("Step ", i)
+    for i in 1:num_steps
+        println("Step ", i)
 
-    a = action(mcts_planner, s)
-    println("Action a: ", a)
-    push!(action_order, a)
-    if metrics[:total_steps] == 0
+        a = action(mcts_planner, s)
+        println("Action a: ", a)
         metrics[:total_cost] += mdp.action_cost[a.action]
+
+        s, r = @gen(:sp, :r)(mdp, s, a, mcts_planner.rng)
+        println("Received reward: ", r)
+        metrics[:total_reward] += r
+        metrics[:discounted_reward] += mdp.discount_factor ^ (i-1) * r
+
+        println("New state sp: ", s)
+        if metrics[:total_steps] == 0 && s.budget <= 0
+            metrics[:total_steps] = i
+        end
+
+        println()
     end
 
-    global s, r = @gen(:sp, :r)(mdp, s, a, mcts_planner.rng)
-    println("Received reward: ", r)
-    metrics[:total_reward] += r
-    metrics[:discounted_reward] += mdp.discount_factor ^ (i-1) * r
+    # Compute metrics
+    metrics[:reliability] = mdp.reward_reliability(s.g)
+    metrics[:max_degree] = maximum(degree(s.g))
+    metrics[:final_graph] = copy(s.g)
 
-    println("New state sp: ", s)
-    if metrics[:total_steps] == 0 && s.budget == 0
-        metrics[:total_steps] = i
+    return metrics
+end
+
+# Evaluate MDP with different hyperparameters
+function evaluate_mdp_2()
+    alpha_r_list = [2.0, 5.0, 8.0]
+    budget_list = [10, 20, 50]
+    num_steps = 20
+
+    experiments = Dict()
+    
+    for alpha_r in alpha_r_list
+        for budget in budget_list
+            mdp = EnergyGridMDP(reward_alpha=alpha_r)
+            s0 = EnergyGridState(SimpleGraph(), budget)     # Budget
+            println("New run")
+            println("Alpha_r = ", alpha_r, ". Budget = ", budget)
+            metrics = evaluate_mdp_2a(mdp, s0, num_steps)
+            println("Metrics:")
+            println(metrics)
+            experiments[(alpha_r, budget)] = metrics
+            println()
+        end
     end
 
     println()
+    println("Experiments: ")
+    println(experiments)
 end
 
-# Compute metrics
-metrics[:reliability] = mdp.reward_reliability(s.g)
-# metrics[:max_degree] = 
+evaluate_mdp_2()
 
-println("List of actions: ")
-println(action_order)
-# println([(i, a.action) for (i, a) in enumerate(action_order)])
 
-println("Metrics: ")
-println(metrics)
+""" Testing v2 """
+function evaluate_mdp_1()
+    mdp = EnergyGridMDP(reward_alpha=30.0, reward_beta=4.0)
+    solver = MCTSSolver(n_iterations=200, depth=20, exploration_constant=10.0)
+    mcts_planner = POMDPs.solve(solver, mdp)
 
-println("Degrees: ", degree(s.g))
+    s0 = EnergyGridState(SimpleGraph(), 100)     # Budget
+    num_steps = 100
+    s = s0
 
+    # Metrics to track
+    metrics::Dict{Symbol, Any} = Dict(
+        :reliability => 0,
+        :total_cost => 0,
+        :total_steps => 0,
+        :max_degree => 0,
+        :total_reward => 0,
+        :discounted_reward => 0    
+    )
+
+    action_order = Vector{EnergyGridAction}()
+
+    for i in 1:num_steps
+        println("Step ", i)
+
+        a = action(mcts_planner, s)
+        println("Action a: ", a)
+        push!(action_order, a)
+        if metrics[:total_steps] == 0
+            metrics[:total_cost] += mdp.action_cost[a.action]
+        end
+
+        global s, r = @gen(:sp, :r)(mdp, s, a, mcts_planner.rng)
+        println("Received reward: ", r)
+        metrics[:total_reward] += r
+        metrics[:discounted_reward] += mdp.discount_factor ^ (i-1) * r
+
+        println("New state sp: ", s)
+        if metrics[:total_steps] == 0 && s.budget <= 0
+            metrics[:total_steps] = i
+        end
+
+        println()
+    end
+
+    # Compute metrics
+    metrics[:reliability] = mdp.reward_reliability(s.g)
+    metrics[:max_degree] = maximum(degree(s.g))
+
+    println("List of actions: ")
+    println(action_order)
+    # println([(i, a.action) for (i, a) in enumerate(action_order)])
+
+    println("Metrics: ")
+    println(metrics)
+
+    println("Degrees: ", degree(s.g))
+    
+end
 
 """Initial testing"""
-# # print(actions(mdp, EnergyGridState(SimpleGraph(5))))
+function evaluate_mdp_0()
+    # print(actions(mdp, EnergyGridState(SimpleGraph(5))))
 
-# solver = MCTSSolver(n_iterations=100, depth=10, exploration_constant=10.0)
-# mcts_planner = POMDPs.solve(solver, mdp)
+    solver = MCTSSolver(n_iterations=100, depth=10, exploration_constant=10.0)
+    mcts_planner = POMDPs.solve(solver, mdp)
 
-# s0 = EnergyGridState(SimpleGraph(5), 12)
-# s = s0
-# total_reward = 0
-# num_steps = 10
-# action_order = Vector{EnergyGridAction}()
-# for i in 1:num_steps
-#     println("Step ", i)
-#     a = action(mcts_planner, s)
-#     println("Action a: ", a)
-#     push!(action_order, a)
-#     global s, r = @gen(:sp, :r)(mdp, s, a, mcts_planner.rng)
-#     global total_reward += r
-#     println("New state sp: ", s)
-#     println()
-# end
+    s0 = EnergyGridState(SimpleGraph(5), 12)
+    s = s0
+    total_reward = 0
+    num_steps = 10
+    action_order = Vector{EnergyGridAction}()
+    for i in 1:num_steps
+        println("Step ", i)
+        a = action(mcts_planner, s)
+        println("Action a: ", a)
+        push!(action_order, a)
+        global s, r = @gen(:sp, :r)(mdp, s, a, mcts_planner.rng)
+        global total_reward += r
+        println("New state sp: ", s)
+        println()
+    end
 
-# println("List of actions: ", [(i, a.action) for (i, a) in enumerate(action_order)])
-# println("Total reward: ", total_reward)
+    println("List of actions: ", [(i, a.action) for (i, a) in enumerate(action_order)])
+    println("Total reward: ", total_reward)
 
 # Debugging outputs:
 # print(mcts_planner.rng)
 # print(@gen(:sp, :r)(mdp, s0, EnergyGridAction(:addSupply, nothing), mcts_planner.rng))
+end
---
+++ plot_graph.jl
@@ -0,0 +1,22 @@
+"""
+Plot graphs
+"""
+
+using GraphPlot         # for plotting graphs
+using Compose, Cairo, Fontconfig    # for saving graphs
+
+# Experiment Initial results
+experiments_initial = Dict{Any, Any}((2.0, 50) => Dict{Symbol, Any}(:max_degree => 1, :total_cost => 24, :discounted_reward => 45.428202133655056, :total_steps => 0, :final_graph => SimpleGraph{Int64}(1, [Int64[], Int64[], Int64[], Int64[], Int64[], Int64[], Int64[], [10], Int64[], [8], Int64[], Int64[], Int64[], Int64[]]), :total_reward => 149.0, :reliability => 0), (5.0, 10) => Dict{Symbol, Any}(:max_degree => 1, :total_cost => 25, :discounted_reward => 38.11413417866016, :total_steps => 6, :final_graph => SimpleGraph{Int64}(1, [[2], [1]]), :total_reward => 112.0, :reliability => 1), (8.0, 20) => Dict{Symbol, Any}(:max_degree => 1, :total_cost => 31, :discounted_reward => 63.26233454094308, :total_steps => 10, :final_graph => SimpleGraph{Int64}(1, [[2], [1]]), :total_reward => 173.0, :reliability => 1), (2.0, 10) => Dict{Symbol, Any}(:max_degree => 0, :total_cost => 20, :discounted_reward => 38.45760313375447, :total_steps => 10, :final_graph => SimpleGraph{Int64}(0, [Int64[], Int64[], Int64[], Int64[], Int64[], Int64[], Int64[], Int64[]]), :total_reward => 118.0, :reliability => 0), (5.0, 20) => Dict{Symbol, Any}(:max_degree => 2, :total_cost => 28, :discounted_reward => 30.440225077754462, :total_steps => 14, :final_graph => SimpleGraph{Int64}(2, [[2, 3], [1], [1]]), :total_reward => 104.0, :reliability => 1), (8.0, 50) => Dict{Symbol, Any}(:max_degree => 1, :total_cost => 30, :discounted_reward => 63.26233454094308, :total_steps => 0, :final_graph => SimpleGraph{Int64}(1, [[2], [1]]), :total_reward => 173.0, :reliability => 1), (2.0, 20) => Dict{Symbol, Any}(:max_degree => 0, :total_cost => 22, :discounted_reward => 37.81956597056589, :total_steps => 18, :final_graph => SimpleGraph{Int64}(0, [Int64[], Int64[], Int64[], Int64[], Int64[], Int64[], Int64[], Int64[], Int64[], Int64[], Int64[]]), :total_reward => 126.0, :reliability => 0), (8.0, 10) => Dict{Symbol, Any}(:max_degree => 1, :total_cost => 24, :discounted_reward => 44.36251454094308, :total_steps => 7, :final_graph => SimpleGraph{Int64}(1, [[2], [1]]), :total_reward => 145.0, :reliability => 1), (5.0, 50) => Dict{Symbol, Any}(:max_degree => 2, :total_cost => 42, :discounted_reward => 57.38177985322602, :total_steps => 0, :final_graph => SimpleGraph{Int64}(3, [[2, 3], [1, 3], [1, 2]]), :total_reward => 181.0, :reliability => 2))
+
+alpha_r_list = [2.0, 5.0, 8.0]
+budget_list = [10, 20, 50]
+
+for alpha_r in alpha_r_list
+    for budget in budget_list
+        # Plot each graph
+        # plot = gplot(experiments_initial[(alpha_r, budget)][:final_graph], layout=circular_layout)
+        # draw(PDF(string("./experiment_initial/graph_alpha_r_", alpha_r, "_budget_", budget, ".pdf"), 16cm, 16cm), plot)
+    end
+end
+
+# write_gph(K2_G_best, node_names, string("project1/outputs_", dataset, "/K2_best_G_order_random.gph"))
